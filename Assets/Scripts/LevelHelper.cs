364f7b8aaf9b91533dbd8a06993cc460a12bd939
liamvt <liamvantoorenburg@gmail.com>
2017-05-28 19:20:06 +0200

    sprint2

---
+++ Assets/Scripts/LevelHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelHelper : MonoBehaviour {
+
+	public int currentLevel = 1;
+	public int enemySpawnRate = 10;
+    public int foodSpawnRate = 10;
+    public int numRooms = 2;
+
+    public string[] action;
+
+
+    void Start () {
+		DontDestroyOnLoad (this);
+
+		if (GameObject.FindGameObjectsWithTag ("LevelHelper").Length > 1) {
+			Destroy (gameObject);
+		}
+
+	}
+
+}
